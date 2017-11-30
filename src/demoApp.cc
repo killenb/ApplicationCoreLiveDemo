80b6cee9152477269a955b487b8462d47bdf0b25
Martin Hierholzer <martin.hierholzer@desy.de>
2017-11-30 17:24:20 +0100

    added ControlerModule

--- src/demoApp.cc
+++ src/demoApp.cc
@@ -12,9 +12,30 @@
 
 namespace ctk = ChimeraTK;
 
+struct Controller : public ctk::ApplicationModule {
+    using ctk::ApplicationModule::ApplicationModule;
+
+    ctk::ScalarPollInput<double> temperatureSetpoint{this, "temperatureSetpoint", "degC", "The setpoint for the temperature control"};
+    ctk::ScalarPushInput<double> temperatureReadback{this, "temperatureReadback", "degC", "The measured temperature"};
+    ctk::ScalarOutput<double> heatingCurrent{this, "heatingCurrent", "mA", "Supply current for the heating device"};
+    
+    void mainLoop() {
+      const double gain = 100.0;
+
+      while(true) {
+        temperatureReadback.read();
+        temperatureSetpoint.readNonBlocking();
+        heatingCurrent = gain * (temperatureSetpoint - temperatureReadback);
+        heatingCurrent.write();
+      }
+    }
+};
+
 struct ExampleApp : public ctk::Application {
     ExampleApp() : Application("exampleApp") {}
     ~ExampleApp() { shutdown(); }
+
+    Controller controller{this, "controller", "Proportional controller for temperature"};
 
     ctk::DeviceModule dev{"Heater","heater"};
     ctk::DeviceModule timer{"Timer"};
@@ -29,8 +50,9 @@ void ExampleApp::defineConnections() {
 
     auto triggerNr = timer("triggerNr", typeid(int), 1, ctk::UpdateMode::push);
 
-    cs("heatingCurrent", typeid(int), 1) >> dev("heatingCurrent");
-    dev("temperatureReadback", typeid(double), 1) [ triggerNr ]
+    cs("temperatureSetpoint") >> controller.temperatureSetpoint;
+    controller.heatingCurrent >> dev("heatingCurrent");
+    dev("temperatureReadback") [ triggerNr ] >> controller.temperatureReadback
         >> cs("temperatureReadback");
 }
 
